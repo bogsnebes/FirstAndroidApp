8541b28b2eab90d342fa70386ce128b1f7d4f459
MutterGott <bogdan.bogotov123123@gmail.com>
2021-05-24 19:42:22 +0300

    Created the work semafor

--- app/src/main/java/com/example/myapplicationtwo/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationtwo/MainActivity.kt
@@ -1,39 +1,57 @@
 package com.example.myapplicationtwo
 
+import android.app.Activity
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import java.util.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    private var counter: Int = 0
-    private var text: String = "PopkaAss $counter"
+    private lateinit var semafor: ImageView
+    private lateinit var semaforButton: ImageButton
+    private lateinit var timer: Timer
 
-    private var cLayout: ConstraintLayout? = null
-    private lateinit var tvText: TextView
+    private var imageArray: IntArray = intArrayOf(R.drawable.semafor_red, R.drawable.semafor_yellow, R.drawable.semafor_green)
+
+    private var isRun: Boolean = true
+    private var counter: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        semafor = findViewById(R.id.Semafor)
+    }
 
-        tvText = findViewById(R.id.tvText)
-        val subText_1: String = text.substringAfter('a')
-        val subText_2: String = subText_1.substringBefore(' ')
-        tvText.text = subText_2
+    fun onClickStartStop(view: View) {
+        view as ImageView
+        if (isRun) {
+            startStop()
+            view.setImageResource(R.drawable.button_stop)
+            isRun = false
+        }
+        else {
+            view.setImageResource(R.drawable.button_start)
+            semafor.setImageResource(R.drawable.semafor_grey)
+            isRun = true
+            timer.cancel()
+            counter = 0
+        }
+    }
 
-        Thread {
-            while (counter < 5) {
-                Thread.sleep(5000)
-                runOnUiThread {
-                    tvText.text = counter.toString()
-                    if (counter == 5) {
-                        cLayout = findViewById(R.id.cLayout)
-                        cLayout?.setBackgroundColor(Color.BLACK)
-                        tvText.setHintTextColor(Color.WHITE)
-                    }
-                    counter++
-                }
+    fun startStop() {
+        timer = Timer()
+        timer.schedule(object :TimerTask() {
+            override fun run() {
+            runOnUiThread {
+                semafor.setImageResource(imageArray[counter])
+                counter++
+                if (counter == 3) counter = 0
             }
-        }.start()
+            }
+        }, 0, 1000)
     }
 }
