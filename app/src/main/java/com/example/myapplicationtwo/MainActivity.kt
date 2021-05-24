c77572195ce90d431768211369bd0730485aa09b
MutterGott <bogdan.bogotov123123@gmail.com>
2021-05-24 22:48:37 +0300

    Refactor code

--- app/src/main/java/com/example/myapplicationtwo/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationtwo/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.example.myapplicationtwo
 
-import android.app.Activity
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import java.util.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var semafor: ImageView
-    private lateinit var semaforButton: ImageButton
-    private lateinit var timer: Timer
-
-    private var imageArray: IntArray = intArrayOf(R.drawable.semafor_red, R.drawable.semafor_yellow, R.drawable.semafor_green)
 
     private var isRun: Boolean = true
     private var counter: Int = 0
@@ -32,8 +23,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             startStop()
             view.setImageResource(R.drawable.button_stop)
             isRun = false
-        }
-        else {
+        } else {
             view.setImageResource(R.drawable.button_start)
             semafor.setImageResource(R.drawable.semafor_grey)
             isRun = true
@@ -43,15 +33,20 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 
     fun startStop() {
-        timer = Timer()
-        timer.schedule(object :TimerTask() {
+        timer.schedule(object : TimerTask() {
             override fun run() {
-            runOnUiThread {
-                semafor.setImageResource(imageArray[counter])
+                runOnUiThread {
+                    semafor.setImageResource(imageArray[counter])
+                }
                 counter++
                 if (counter == 3) counter = 0
             }
-            }
         }, 0, 1000)
+    }
+
+    companion object {
+        private val timer: Timer = Timer()
+        private val imageArray: IntArray =
+            intArrayOf(R.drawable.semafor_red, R.drawable.semafor_yellow, R.drawable.semafor_green)
     }
 }
